b64bde57d8dafb10930e6118bc400d7e95aae924
extslayer <kabirismanmohan@gmail.com>
2024-11-11 21:54:29 +0530

    added changes acc to bmi

---
+++ BMI Calculator/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Manmohan Shrivastava on 10/11/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/bmi.swift
@@ -0,0 +1,9 @@
+//
+//  bmi.swift
+//  BMI Calculator
+//
+//  Created by Manmohan Shrivastava on 11/11/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
